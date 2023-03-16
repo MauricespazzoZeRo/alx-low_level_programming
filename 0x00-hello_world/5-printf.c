013df9c130425e111cd451ed47804d2e2af1e9cd
E. Maurice Umanah <mauricejnr161@gmail.com>
2023-03-16 12:53:22 -0700

    Print a text with the "printf" function

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 /**
+ * printf - Print a text as inputted
+ * main - Entry point
  * Description: Use function printf to print
  * Return: Anything
  */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,");
+printf("with proper grammar, but the outcome is a piece of art, ");
 return (0);
 }
