ffbee32996aa9877240737bfbf2660748b36e58c
E. Maurice Umanah <mauricejnr161@gmail.com>
2023-03-17 11:00:43 -0700

    Print alphaBETS with "putchar" function

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/* Input other headers here */
+
+/**
+ * main - Entry Point
+ *
+ * This program will print the alphabets
+ * in lowercase with a new line.
+ *
+ * Return: 0 for successful execution
+ */
+int main(void)
+{
+	char alpha;
+
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+	{
+		putchar(alpha);
+	}
+	
+	for (alpha = 'A'; alpha <= 'Z'; alpha++)
+	{
+		putchar(alpha);
+	}
+	putchar('\n');
+
+	return (0);
+}
