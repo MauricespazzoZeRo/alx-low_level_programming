483860fcf9526da6f2651aa09f7f1c37b40994f9
E. Maurice Umanah <mauricejnr161@gmail.com>
2023-03-17 12:18:04 -0700

    Print numbers from 0-9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/* Input other headers here */
+
+/**
+ * main - Entry Point
+ *
+ * This program will print numbers from
+ * 0-9 with a new line.
+ *
+ * Return: 0 for successful execution
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num <= 9; num++)
+	{
+		printf("%d", num);
+	}
+
+	printf("\n");
+
+	return (0);
+}
