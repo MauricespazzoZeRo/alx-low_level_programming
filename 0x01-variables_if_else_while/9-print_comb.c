1f976ce3668da39ad9c55a314076a331e4fe4159
E. Maurice Umanah <mauricejnr161@gmail.com>
2023-03-17 15:31:40 -0700

    Print all possible combinations of single-digit numbers

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -5,28 +5,25 @@
  * main - Entry Point
  *
  * This program will print all possible
- * combinations of single-digit numbers using function 
+ * combinations of single-digit numbers using function
  * "putchar" with a new line.
  *
  * Return: 0 for successful execution
  */
 int main(void)
 {
-	int sin1, sin2;
+	int sin1;
 
-	for (sin1 = 0; sin1 < 10; sin1++)
+	for (sin1 = 0; sin1 <= 9; sin1++)
 	{
-		for (sin2 = sin1; sin2 < 10; sin2++)
-		{
 		putchar(sin1 + '0');
-		putchar(sin2 + '0');
-			if (sin1 != 9 || sin2 != 9)
-			{
-				putchar(',');
-				putchar(' ');
-			}
+		if (sin1 != 9)
+		{
+			putchar(',');
+			putchar(' ');
 		}
 	}
+	putchar('\n');
 
 	return (0);
 }
