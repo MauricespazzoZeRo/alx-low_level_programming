6151247a4eb7f37fa596306e50a2ed2990252582
E. Maurice Umanah <mauricejnr161@gmail.com>
2023-03-17 06:50:21 -0700

    Tell if a random number is positive, negative or, zero

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,7 +3,14 @@
 #include <stdio.h>
 /* more headers goes there */
 
-/* betty style doc for function main goes there */
+/**
+ * main - Entry Point
+ *
+ * This program will assign a random number to the
+ * variable n each time it is executed
+ *
+ * Return: Zero for succesful run
+ */
 int main(void)
 {
 	int n;
@@ -12,16 +19,16 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
 	if (n > 0)
-	  {
-	    printf("%d is positive \n", n);
-	  }
+        {
+	    printf("%d is positive\n", n);
+	}
 	else if (n < 0)
-	  {
-	    printf("%d is negative \n", n);
-	  }
+	{
+	    printf("%d is negative\n", n);
+	}
 	else
-	  {
-	    printf ("%d is zero \n", n);
-	  }
+	{
+	    printf ("%d is zero\n", n);
+	}
 	return (0);
 }
