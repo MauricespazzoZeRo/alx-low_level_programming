ea6e2d0c4d0a33fc60e356027553f68f175ff18d
E. Maurice Umanah <mauricejnr161@gmail.com>
2023-03-17 17:17:56 -0700

    Print all possible combinations of two two-digit numbers

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+/* Input other headers here */
+
+/**
+ * main - Entry Point
+ *
+ * This program will print all possible different
+ * combinations of two double-digit numbers using function
+ * "putchar" with a new line.
+ *
+ * Return: 0 for successful execution
+ */
+int main(void)
+{
+	int sin1, sin2;
+
+	for (sin1 = 0; sin1 < 100; sin1++)
+	{
+		for (sin2 = sin1 + 1; sin2 < 100; sin2++)
+		{
+			putchar((sin1 / 10) + '0');
+			putchar((sin1 % 10) + '0');
+			putchar(' ');
+			putchar((sin2 / 10) + '0');
+			putchar((sin2 % 10) + '0');
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
