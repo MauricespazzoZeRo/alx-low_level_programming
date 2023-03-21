dfc07d91f49f998904f54b32ed0c4a8ae2fb54bb
E. Maurice Umanah <mauricejnr161@gmail.com>
2023-03-21 16:14:32 -0700

    Print alpha x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Prints the alphabets x10 in lowercase, followed by a newline.
+ *
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+	char alpha;
+	int alph;
+
+	for (alph = 0; alph <= 10; alph++)
+	{
+		for (alpha = 'a'; alpha <= 'z'; alpha++)
+		{
+			_putchar(alpha);
+		}
+
+		_putchar('\n');
+	}
+}
