97dbbc41ea6f75749ca12007c396e25a2bc2c20e
E. Maurice Umanah <mauricejnr161@gmail.com>
2023-03-21 05:12:09 -0700

    README

---
+++ 0x02-functions_and_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/* Other header files */
+
+/**
+ * main - Entry Point
+ *
+ * Description: This program will print the name
+ * of a function using function "putchar"
+ *
+ * return: 0 on successful run of program
+ */
+int main(void)
+{
+	putchar
---
+++ 0x02-functions_and_nested_loops/README.md
@@ -0,0 +1,3 @@
+#**ALX LOW-LEVEL PROGRAMMING**
+#Author: ***E. Maurice Umanah***
+This directory contains scripts on functions and nested loops in low-level programming made during my time learning with ALX-SE program.
