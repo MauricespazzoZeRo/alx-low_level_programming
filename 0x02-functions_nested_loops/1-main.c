827545cffe8091573adb15fdff7c06a82bfc86e5
E. Maurice Umanah <mauricejnr161@gmail.com>
2023-03-21 15:47:26 -0700

    Update header file

--- 0x02-functions_nested_loops/1-main.c
+++ 0x02-functions_nested_loops/1-main.c
@@ -5,10 +5,8 @@
  *
  * Return: Always 0.
  */
-void print_alphabet(void);
-
 int main(void)
 {
-    print_alphabet();
-    return (0);
+	print_alphabet();
+	return (0);
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -5,4 +5,39 @@
 /* Declaration of _putchar function */
 int _putchar(char c);
 
+/* Declaration of print_alphabet function */
+void print_alphabet(void);
+
+/* Declaration of print_alphabet_x10 function */
+void print_alphabet_x10(void);
+
+/* Declaration of _islower function */
+int _islower(int c);
+
+/* Declaration of _isalpha(int c) */
+int _isalpha(int c);
+
+/*Declaration of print_sign function */
+int print_sign(int n);
+
+/* Declaration of _abs function */
+int _abs(int);
+
+/* Declaration of print_last_digit */
+int print_last_digit(int);
+
+/* Declaration of jack_bauer */
+void jack_bauer(void);
+
+/* Declaration of times_table */
+void times_table(void);
+
+/* Declaration of add */
+int add(int, int);
+
+/* Declaration of print_to_98 */
+void print_to_98(int n);
+
+/* Declaration of print_times_table */
+void print_times_table(int n);
 #endif /* MAIN_H */
