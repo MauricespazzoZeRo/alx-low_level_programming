15405a3475a7b1b01d8a0b1631cc50a896514510
E. Maurice Umanah <mauricejnr161@gmail.com>
2023-03-21 13:33:02 -0700

    Update alphabets

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,21 +1,3 @@
-#include "main.h"
-
-/**
- * main -  Entry Point
- *
- * This is a function that prints the alphabets
- * in lowercase, followed by a newline.
- *
- * Return: 0 on successsful run of program
- */
-void print_alphabet(void);
-
-int main(void)
-{
-	print_alphabet();
-	return (0);
-}
-
 /**
  * print_alphabet - Prints the alphabets in lowercase, followed by a newline.
  *
