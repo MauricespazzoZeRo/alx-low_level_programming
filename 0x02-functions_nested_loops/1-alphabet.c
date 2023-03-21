fb02c2fb949b3636caa3fd9c2ddf690633b46c75
E. Maurice Umanah <mauricejnr161@gmail.com>
2023-03-21 14:56:30 -0700

    Update alpha

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,28 +5,15 @@
  *
  * Return: void
  */
+
 void print_alphabet(void)
 {
-    char alpha;
+	char alpha;
 
-    for (alpha = 'a'; alpha <= 'z'; alpha++)
-    {
-        _putchar(alpha);
-    }
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+	{
+		_putchar(alpha);
+	}
 
-    _putchar('\n');
-}
-
-/**
- * main - Entry Point
- *
- * This is a function that prints the alphabets
- * in lowercase, followed by a newline.
- *
- * Return: 0 on successful run of program
- */
-int main(void)
-{
-    print_alphabet();
-    return (0);
+	_putchar('\n');
 }
