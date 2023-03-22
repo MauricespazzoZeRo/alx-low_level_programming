d2badfabeca2256f4be534802305ca81fedbc987
E. Maurice Umanah <mauricejnr161@gmail.com>
2023-03-22 07:51:27 -0700

    Print last digit of a number

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -11,19 +11,19 @@ int print_last_digit(int n)
 {
 	int last_digit;
 
-	if (n > 0)
-	{
-		last_digit = n % 10;
-	}
 	if (n < 0)
 	{
-		n = -n;
+		n = -n; /* convert negative number to positive */
 	}
-	else if (n == 0)
+
+	if (n > 0)
+	{
+		last_digit = n % 10; /* obtain last digit using modulo operator */
+	}
+	else
 	{
 		last_digit = 0;
 	}
-
 	_putchar(last_digit + '0');
 
 	return (last_digit);
