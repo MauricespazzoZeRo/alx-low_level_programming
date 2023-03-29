6a9e0593e5ff8820a0d440676d20f02abab9228f
MauricespazzoZeRo <mauricejnr161@gmail.com>
2023-03-29 11:27:19 +0000

    Print integers with _putchar

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_number - prints an integer
+ *
+ * @n: the integer to be printed
+ */
+void print_number(int n)
+{
+	int div = 1;
+
+	if (n < 0) {
+		_putchar('-');
+		n = -n;
+	}
+
+	while (n / div >= 10) {
+		div *= 10;
+	}
+
+	while (div != 0) {
+		int digit = n / div;
+		_putchar(digit + '0');
+		n %= div;
+		div /= 10;
+	}
+}
