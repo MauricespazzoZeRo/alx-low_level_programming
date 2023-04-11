30711d9ee81b8d1df0dec7d077d97cc967785fa1
MauricespazzoZeRo <mauricejnr161@gmail.com>
2023-04-11 15:18:33 +0000

    Give back some change

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -10,52 +10,52 @@
 
 int main(int argc, char *argv[])
 {
-    int cents, coins = 0;
+	int cents, coins = 0;
 
-    if (argc != 2)
-    {
-        printf("Error\n");
-        return 1;
-    }
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
 
-    cents = atoi(argv[1]);
+	cents = atoi(argv[1]);
 
-    if (cents < 0)
-    {
-        printf("0\n");
-        return 0;
-    }
+	if (cents < 0)
+	{
+		printf("0\n");
+		return (0);
+	}
 
-    while (cents > 0)
-    {
-        if (cents >= 25)
-        {
-            coins += cents / 25;
-            cents %= 25;
-        }
-        else if (cents >= 10)
-        {
-            coins += cents / 10;
-            cents %= 10;
-        }
-        else if (cents >= 5)
-        {
-            coins += cents / 5;
-            cents %= 5;
-        }
-        else if (cents >= 2)
-        {
-            coins += cents / 2;
-            cents %= 2;
-        }
-        else
-        {
-            coins += cents;
-            cents = 0;
-        }
-    }
+	while (cents > 0)
+	{
+		if (cents >= 25)
+		{
+			coins += cents / 25;
+			cents %= 25;
+		}
+		else if (cents >= 10)
+		{
+			coins += cents / 10;
+			cents %= 10;
+		}
+		else if (cents >= 5)
+		{
+			coins += cents / 5;
+			cents %= 5;
+		}
+		else if (cents >= 2)
+		{
+			coins += cents / 2;
+			cents %= 2;
+		}
+		else
+		{
+			coins += cents;
+			cents = 0;
+		}
+	}
 
-    printf("%d\n", coins);
+	printf("%d\n", coins);
 
-    return 0;
+	return (0);
 }
