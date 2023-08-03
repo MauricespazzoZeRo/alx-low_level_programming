7a2caa856c02f5b1f433cd113414c6fdff9de837
MauricespazzoZeRo <mauricejnr161@gmail.com>
2023-08-03 18:20:03 +0000

    Bettified

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -9,26 +9,26 @@
 
 void print_binary(unsigned long int n)
 {
-    unsigned long int u_num = 1UL << (sizeof(unsigned long int) * 8 - 1);
-    int flag = 0;
+	unsigned long int u_num = 1UL << (sizeof(unsigned long int) * 8 - 1);
+	int flag = 0;
 
-    while (u_num > 0)
-    {
-        if ((n & u_num) == u_num)
-        {
-            flag = 1;
-            _putchar('1');
-        }
-        else if (flag)
-        {
-            _putchar('0');
-        }
+	while (u_num > 0)
+	{
+		if ((n & u_num) == u_num)
+		{
+			flag = 1;
+			_putchar('1');
+		}
+		else if (flag)
+		{
+			_putchar('0');
+		}
 
-        u_num >>= 1;
-    }
+		u_num >>= 1;
+	}
 
-    if (!flag)
-    {
-        _putchar('0');
-    }
+	if (!flag)
+	{
+		_putchar('0');
+	}
 }
