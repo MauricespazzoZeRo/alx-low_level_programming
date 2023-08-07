9e4f41f652bb2540b2f6a1a83500c7efe7ef7e77
MauricespazzoZeRo <mauricejnr161@gmail.com>
2023-08-07 18:39:35 +0000

    Cp

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -3,6 +3,89 @@
 #define BUFFER_SIZE 1024
 
 /**
+ * print_usage_and_exit - Prints the usage message and exits
+ *						  with code 97.
+ */
+void print_usage_and_exit(void)
+{
+	dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+	exit(97);
+}
+
+/**
+ * error_and_exit - Prints an error message and exits with
+ *					the appropriate exit code.
+ * @msg: The error message.
+ * @file_name: The name of the file associated with the error.
+ */
+void error_and_exit(const char *msg, const char *file_name)
+{
+	dprintf(STDERR_FILENO, "Error: %s %s\n", msg, file_name);
+	exit(99);
+}
+
+/**
+ * close_fd_and_exit - Prints an error message for closing
+ *					   a file descriptor and exits with code 100.
+ *
+ * @fd: The file descriptor associated with the error.
+ */
+void close_fd_and_exit(int fd)
+{
+	dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+	exit(100);
+}
+
+/**
+ * copy_file - Copies the content of one file to another file.
+ * @file_from: The name of the source file.
+ * @file_to: The name of the destination file.
+ */
+void copy_file(const char *file_from, const char *file_to)
+{
+	int fd_from, fd_to;
+	ssize_t bytes_read, bytes_written;
+	char buffer[BUFFER_SIZE];
+	mode_t permit = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
+
+	fd_from = open(file_from, O_RDONLY);
+	if (fd_from == -1)
+	{
+		error_and_exit("Can't read from file", file_from);
+	}
+
+	fd_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, permit);
+	if (fd_to == -1)
+	{
+		error_and_exit("Can't write to", file_to);
+	}
+
+	while ((bytes_read = read(fd_from, buffer, BUFFER_SIZE)) > 0)
+	{
+		bytes_written = write(fd_to, buffer, bytes_read);
+		if (bytes_written == -1)
+		{
+			error_and_exit("Can't write to", file_to);
+		}
+	}
+
+	if (bytes_read == -1)
+	{
+		error_and_exit("Can't read from file", file_from);
+	}
+
+	if (close(fd_from) == -1)
+	{
+		close_fd_and_exit(fd_from);
+	}
+
+	if (close(fd_to) == -1)
+	{
+		close_fd_and_exit(fd_to);
+	}
+}
+
+/**
  * main - Entry point for program to copy one file to another
  * @ac: Number of arguments passed to program
  * @av: Array of arguments passed to program
@@ -10,65 +93,14 @@
  * Return: 0 on success, otherwise 97, 98, 99, or 100 if an error occurs
  */
 
-int main(int argc, char **argv)
+int main(int ac, char *av[])
 {
-	int fd_from, fd_to, num_bytes_read, num_bytes_written;
-	char buffer[BUFFER_SIZE];
-	mode_t file_permissions = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
-
-	if (argc != 3)
+	if (ac != 3)
 	{
-		dprintf(STDERR_FILENO, "Usage: %s file_from file_to\n", argv[0]);
-		return (EXIT_FAILURE);
+		print_usage_and_exit();
 	}
 
-	fd_from = open(argv[1], O_RDONLY);
-	if (fd_from == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		return (EXIT_FAILURE);
-	}
+	copy_file(av[1], av[2]);
 
-	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, file_permissions);
-	if (fd_to == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		close(fd_from);
-		return (EXIT_FAILURE);
-	}
-
-	while ((num_bytes_read = read(fd_from, buffer, BUFFER_SIZE)) > 0)
-	{
-		num_bytes_written = write(fd_to, buffer, num_bytes_read);
-		if (num_bytes_written != num_bytes_read)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-			close(fd_from);
-			close(fd_to);
-			return (EXIT_FAILURE);
-		}
-	}
-
-	if (num_bytes_read == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		close(fd_from);
-		close(fd_to);
-		return (EXIT_FAILURE);
-	}
-
-	if (close(fd_from) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
-		close(fd_to);
-		return (EXIT_FAILURE);
-	}
-
-	if (close(fd_to) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
-		return (EXIT_FAILURE);
-	}
-
-	return (EXIT_SUCCESS);
+	return (0);
 }
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -17,4 +17,12 @@ int create_file(const char *filename, char *text_content);
 
 int append_text_to_file(const char *filename, char *text_content);
 
+void print_usage_and_exit(void);
+
+void error_and_exit(const char *msg, const char *file_name);
+
+void close_fd_and_exit(int fd);
+
+void copy_file(const char *file_from, const char *file_to);
+
 #endif /* MAIN_H */
